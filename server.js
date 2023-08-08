001f037cdfd3795d24a9d6f5f82994b4d51fbab1
NamelessExplorer <91269749+NamelessExplorer@users.noreply.github.com>
2023-08-08 16:49:49 +0530

    using env vars

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const express = require('express')
 const app = express();
 app.use(router)
 
-const PORT=3000;
+const PORT= process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Local app listening on port:', PORT))
