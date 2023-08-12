7687ef9a8cce05ddeafd286ed8edaf7cdbc32456
NamelessExplorer <91269749+NamelessExplorer@users.noreply.github.com>
2023-08-12 23:50:22 +0530

    Updated server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -2,6 +2,14 @@ const router = require('./api')
 const express = require('express')
 
 const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+  
 app.use(router)
 
 const PORT= process.env.PORT || 3000;
