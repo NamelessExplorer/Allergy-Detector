ee80f9009bc88514656123b1c17d237f91ab8bf6
NamelessExplorer <91269749+NamelessExplorer@users.noreply.github.com>
2023-08-13 10:30:05 +0530

    Tried enabling CORS access on server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express')
 
 const app = express();
 
+const cors = require('cors');
+app.use(cors())
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
